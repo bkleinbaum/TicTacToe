84a00bc03dbeff69259bd2a46010b3ea421171ef
Sophie <sophieadamswastaken@gmail.com>
2015-08-07 10:52:47 -0400

    updating all of my changes

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,73 +1,73 @@
-var Game = function(e) {
-	this.element = e;
+var Game = function(e) { //my game is initiliazing itself
+	this.element = e; //this is that!!
 }
 
-var xSpot = "X";
-var oSpot = "O";
-var marker = xSpot;
-var plays = 0;
+var xSpot = "X"; //xSpot is where the X will live
+var oSpot = "O"; //oSpot is where the O will live
+var marker = xSpot; //marker represents each click/choice of the FIRSTUSER
+var plays = 0; //switch back and forth between players in order to check the moves
 
-var switchPlayer = function() {
-	plays ++;
-	if(marker == xSpot) {
-		marker = oSpot;
-	} else {
-		marker = xSpot;
+var switchPlayer = function() { //this is how we switch players, FIRSTUSER is set to X by default
+	plays ++; //in order to check our results, we have to go back and forth
+	if(marker == xSpot) { //after every X move
+		marker = oSpot; //change the marker so O can make a move
+	} else { //if O has made a move
+		marker = xSpot; //switch the status back
 	}
 };
 
-function div() {
-var div = $('body').append('<div id="board"><tbody>');
+function div() { //our game board
+var div = $('body').append('<div id="board">');
 $('#board').append("<div id='0' class='cell'></div><div id='1' class='cell'></div><div id='2' class='cell'></div>");
 $('#board').append("<div id='3' class='cell'></div><div id='4' class='cell'></div><div id='5' class='cell'></div>");
 $('#board').append("<div id='6' class='cell'></div><div id='7' class='cell'></div><div id='8' class='cell'></div>");
 return div;
-}
-
-Game.prototype.oneGo = function() {
-	this.element.addClass(marker);
-	this.element.text(marker);
-	gameState();
-	switchPlayer();
 };
 
-var gameState = function() {
-	var wins = [
+Game.prototype.oneGo = function() { //this is how we take a turn:
+	this.element.addClass(marker); //for every click, add a marker INTO the class of the div with X or O
+	this.element.html(marker); //change the content of the div to hold the HTML of the marker (X or O)
+	gameState(); //compare all cells with an X or O in the class
+	switchPlayer(); //if there are no wins, switch players
+};
+
+var gameState = function() { //check the current board for potential wins
+	var wins = [ //options are for a 3 by 3 board
 	[0,1,2], [3,4,5], [6,7,8],
 	[0,3,6], [1,4,7], [2,5,8],
 	[0,4,8], [2,4,6]
 	]
-	for (var i = 0; i < wins.length; i++) {
-		if( $('#' + wins[i][0]).text() === marker &&
-		$('#' + wins[i][1]).text() === marker &&
-		$('#' + wins[i][2]).text() === marker ) {
-			alert(marker + " wins");
-			resetGame();
-		}
-		if (plays === 8) {
-			alert("draw");
-			resetGame();
+	for (var i = 0; i < wins.length; i++) { //for every X or O on the board, compare them for wins
+		if( $('#' + wins[i][0]).html() === marker && //first, figure out what the content of #cellid is
+		$('#' + wins[i][1]).html() === marker && //check $('#cellid X') or $('#cellid 0')'s content to see if it matches X or O
+		$('#' + wins[i][2]).html() === marker ) { //against all potential wins
+			alert(marker + " wins"); //if there are three in a row, content of marker (oSpot or Xspot) wins!
+			resetGame(); //then reset the game
+		} //and if that didnt happen
+		if (plays === 8) { //if we hit the max number of boxes and there hasnt been a winner
+			alert("draw"); //it is a draw
+			resetGame(); //so reset the game
 			break;
 		};
-	} $('#masterdiv div').html(''); //when there is a winner or a draw clear all the Xs and Os from the board
+	} $('#div').html(''); //when there is a winner or a draw clear all the Xs and Os from the board
 };
 
 var resetGame = function() {
-	location.reload();
+	location.reload(); //every time we need to reset the gsme, this is what resets it - it just reloads itself back in the same location
 };
 
-Game.prototype.listen = function() {
-	var that = this;
-	this.element.on('click', function(){
-		that.oneGo();
-		$(this).off('click');
+Game.prototype.listen = function() { //this is our clicking part
+	var that = this; //when this is that
+	this.element.on('click', function(){ //on everything clicked within our scope, click!
+		that.oneGo(); //start our first turn! go!
+		$(this).off('click'); //turn off!!!!!!
 	});
 };
 
 $(document).ready(function() {
-  div();
-	$('.cell').each( function() {
-		var cell = new Game($(this));
-		cell.listen();
+  div(); //this creates our table
+	$('.cell').each( function() { //this is listening for clicks on every div cell on my board
+		var cell = new Game($(this)); //whenever cell is clicked on, start a new game
+		cell.listen(); //listen to cells for clicks
 	})
 });
