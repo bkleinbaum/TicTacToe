ef335b21c287ae62a5fce0f27933fa516bfcd1ef
Sophie <sophieadamswastaken@gmail.com>
2015-08-07 12:45:20 -0400

    updated player icons and added sounds

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -5,6 +5,7 @@ var Game = function(e) { //my game is initiliazing itself
 var xSpot = "X"; //xSpot is where the X will live
 var oSpot = "O"; //oSpot is where the O will live
 var marker = xSpot; //marker represents each click/choice of the FIRSTUSER
+var computer = false;
 var plays = 0; //switch back and forth between players in order to check the moves
 
 var switchPlayer = function() { //this is how we switch players, FIRSTUSER is set to X by default
@@ -24,9 +25,11 @@ $('#board').append("<div id='6' class='cell'></div><div id='7' class='cell'></di
 return div;
 };
 
-Game.prototype.oneGo = function() { //this is how we take a turn:
+Game.prototype.oneGo = function() { //this is how we take a turn and assign our X or O:
 	this.element.addClass(marker); //for every click, add a marker INTO the class of the div with X or O
-	this.element.html(marker); //change the content of the div to hold the HTML of the marker (X or O)
+	this.element.data('player', marker); //for every click, add 'player' and X or O
+	this.element.html( "<img src='img/" +marker+ ".png'>") //depending on whether it is X or O, assign the appropriate image
+	//this.element.html(marker); //changed the content of the div to hold the HTML of the marker variable (X or O)
 	gameState(); //compare all cells with an X or O in the class
 	switchPlayer(); //if there are no wins, switch players
 };
@@ -37,13 +40,13 @@ var gameState = function() { //check the current board for potential wins
 	[0,3,6], [1,4,7], [2,5,8],
 	[0,4,8], [2,4,6]
 	]
-	for (var i = 0; i < wins.length; i++) { //for every X or O on the board, compare them for wins
-		if( $('#' + wins[i][0]).html() === marker && //first, figure out what the content of #cellid is
-		$('#' + wins[i][1]).html() === marker && //check $('#cellid X') or $('#cellid 0')'s content to see if it matches X or O
-		$('#' + wins[i][2]).html() === marker ) { //against all potential wins
-			alert(marker + " wins"); //if there are three in a row, content of marker (oSpot or Xspot) wins!
-			resetGame(); //then reset the game
-		} //and if that didnt happen
+	for (var i = 0; i < wins.length; i++) { //for every X or O on the board, compare them for wins: figure out what the data of player is - player X or player O
+		if( $('#' + wins[i][0]).data('player') === marker && //compare first cell of all winning combos with player data X/O
+		$('#' + wins[i][1]).data('player') === marker && //compare second ...
+		$('#' + wins[i][2]).data('player') === marker ) { //basically check against all potential wins ...
+			alert(marker + " wins"); //if there are three of the same player X/O in a row, content of marker (oSpot or Xspot) wins!
+			resetGame(); //then reset the game!
+		} //if that didnt happen,
 		if (plays === 8) { //if we hit the max number of boxes and there hasnt been a winner
 			alert("draw"); //it is a draw
 			resetGame(); //so reset the game
@@ -69,5 +72,9 @@ $(document).ready(function() {
 	$('.cell').each( function() { //this is listening for clicks on every div cell on my board
 		var cell = new Game($(this)); //whenever cell is clicked on, start a new game
 		cell.listen(); //listen to cells for clicks
-	})
+	});
+	$('.cell').on('mouseenter', function() {
+    p = new Audio('raptor.mp3');
+    p.play();
+  });
 });
